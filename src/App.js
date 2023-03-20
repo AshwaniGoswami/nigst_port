6573788d69c1ce52634d9291269b2aaae762e6f4
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-03-20 17:50:52 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home';
-// import Navbar from './components/Navbar';
-// import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Training from './components/NavComponents/Training';
 import Footer from './components/Footer';
 import Copyright from './components/Copyright';
@@ -17,8 +17,8 @@ function App() {
       <Router>
       <Fixed/>
       
-      {/* <Header/>
-      <Navbar/> */}
+      <Header/>
+      <Navbar/>
       
         <Routes>
           <Route path='/' element={<Home />} />
