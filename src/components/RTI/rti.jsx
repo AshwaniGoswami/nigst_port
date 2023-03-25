45ec6e3dd0e3a47c18f00f94b4f3ae425145b87a
Abhishek Paul <phoenixp555@gmail.com>
2023-03-25 12:07:40 +0530

    rti

---
+++ src/components/RTI/rti.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+function RightToInformation() {
+  return (
+    <div className="content-container">
+      <div className="heading-container">
+        <h4>Right To Information</h4>
+      </div>
+
+      <div className="rti-container">
+      <div class="hindi-rti">
+                    <div class="heading-container">
+                    <h4>RTI</h4>
+                </div>
+                    <p> सूचना का अधिकार अधिनियम 2005 से संबंधित किसी भी जानकारी के लिए आप केंद्रीय लोक सूचना अधिकारी,
+                        एनआईजीएसटी से संपर्क कर सकते हैं।</p>
+                    <p> केंद्रीय लोक सूचना अधिकारी का विवरण निम्नानुसार दिया गया है:</p>
+                    <h5> केंद्रीय जन सूचना अधिकारी</h5>
+                    <ul>
+                        <li> श्री ऐ.के. रथ</li>
+                        <li> अधीक्षक सर्वेक्षक (Adhoc )</li>
+                        <li> दूरभाषा सं : 040 – 27201503</li>
+                        <li> फैक्स सं : 040 – 27200286</li>
+                        <li> ई – मैल : iism.soi@gov.in</li>
+                    </ul>
+                    <p>यदि आवेदक संबंधित केंद्रीय लोक अधिकारी द्वारा दिए गए जवाब से संतुष्ट नहीं है तो आप प्रथम अपीलीय
+                        प्राधिकारी को लिख सकते हैं।</p>
+                    <p> प्रथम अपीलीय प्राधिकारी का विवरण निम्नानुसार दिया गया है:</p>
+                    <ul>
+                        <li>प्रथम अपीलीय प्रधिकारी</li>
+                        <li>श्री पी. वी. श्रीनिवास</li>
+                        <li>अपर महासर्वेक्षक</li>
+                        <li>दूरभाषा सं : 040 – 27201507</li>
+                        <li>फैक्स सं : 040 – 27200286</li>
+                        <li>ई – मैल : iism.soi@gov.in</li>
+                    </ul>
+
+                </div>
+        <div id="vertical-line"></div>
+        <div className="english-rti">
+
+          <p>For any information pertaining to Right to Information Act 2005 You may contact Central Public Information Officer, NIGST.</p>
+          <p>The details of Central Public Information officer given as under:</p>
+          <h5>CENTRAL PUBLIC INFORMATION OFFICER</h5>
+          <ul>
+            <li>SHRI A.K. RATH</li>
+            <li>SUPTDG. SURVEYOR (Adhoc)</li>
+            <li>TEL NO. 040 – 27201507</li>
+            <li>FAX NO. 040 – 27200286</li>
+            <li>E-MAIL : iism.soi@gov.in</li>
+          </ul>
+          <p>In case, applicant is not satisfied with the response given by concerned Central Public Officer you may write to first appellate authority.</p>
+          <p>The details of first appellate authority given as under :</p>
+          <ul>
+            <li>FIRST APPELLATE AUTHORITY</li>
+            <li>SHRI P.V. SRINIVAS</li>
+            <li>ADDL. SURVEYOR GENERAL</li>
+            <li>TEL NO. 040 – 27201507</li>
+            <li>FAX NO. 040 – 27200286</li>
+            <li>E-MAIL : iism.soi@gov.in</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RightToInformation;
