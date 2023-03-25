12053d0a2467eb02c3d74a61d8ecdabefb45550b
Abhishek Paul <phoenixp555@gmail.com>
2023-03-25 14:07:52 +0530

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,15 +57,7 @@ const Header = () => {
             <Link to="">Login</Link>
           </div>
           </div>
-          <div className='flex flex-row relative'>
-          <input
-            type="search"
-            name="search"
-            placeholder="Search here.."
-            className="py-2 px-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-full"
-          />
-          <AiOutlineSearch className="h-full absolute right-2" />
-          </div>
+        <Searchbar/>
         </section>
       </div>
     </div>
