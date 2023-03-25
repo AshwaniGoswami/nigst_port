054bd6905d798598f1dee9d72d78f80af45dfb61
Abhishek Paul <phoenixp555@gmail.com>
2023-03-25 17:15:06 +0530

    pand rs images

--- src/components/faculty/FacultyPhotogrammetry.jsx
+++ src/components/faculty/FacultyPhotogrammetry.jsx
@@ -3,12 +3,12 @@ import LeftSection from './LeftSection'
 import LowerLeft from './LowerLeft';
 import LowerRight from './LowerRight';
 import RightSection from './RightSection'
-import imageHead from '../../assests/geodesy/G. Varun Kumar, Head Faculty.JPG'
+import imageHead from '../../assests/photogrammetry/S K Sinha Head Faculty - C,DM&GIS.JPG'
 import DetilnImg from '../pageStructure/DetilnImg';
 
 
 const headData = {
-  name: 'G. Varun Kumar',
+  name: 'S K Sinha',
   designation: 'Deputy Surveyor General',
   position: 'Head Of Faculty',
   image: imageHead
@@ -77,13 +77,13 @@ const Modata = {
 
 const facultyPage={
   images:[
-    { path: require('../../assests/geodesy/Image No. 5.jpg') },
-    { path: require('../../assests/geodesy/Image No. 7.jpg') },
+    { path: require('../../assests/photogrammetry/Dummy Team 2.jpg') },
+    { path: require('../../assests/photogrammetry/Photogramtry.jpg') },
     { path: require('../../assests/geodesy/Snapshot_1420.png') },
     { path: require('../../assests/geodesy/Snapshot_1421.png') }
   ],
   
-  heading:'Faculty P & RS',
+  heading:'Faculty Photogrammetry & RS',
   content:['Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, accusantium, recusandae suscipit minus mollitia tempore amet ducimus dolorum dicta quasi dignissimos voluptatibus. Doloremque quos, non omnis earum saepe placeat nulla necessitatibus fugit ex? Temporibus nisi cum tenetur in alias debitis corporis laborum numquam vel autem. Enim repellat nam doloremque dolorum quia, veritatis autem consequuntur.',' ipsum dolor sit amet consectetur adipisicing elit. Numquam, accusantium, recusandae suscipit minus mollitia tempore amet ducimus dolorum dicta quasi dignissimos voluptatibus. Doloremque quos, non omnis']
 }
 
