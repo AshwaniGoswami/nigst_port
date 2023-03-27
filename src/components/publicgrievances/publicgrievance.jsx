7ea9373acdccd29fdf0550b3f626362be196d727
Abhishek Paul <phoenixp555@gmail.com>
2023-03-27 10:30:08 +0530

    public

---
+++ src/components/publicgrievances/publicgrievance.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const publicgrievance = () => {
+    return (
+        <div class="content-container">
+            <div class="heading-container">
+                <h4>Public Grievence</h4>
+            </div>
+
+            <div class="rti-container">
+                <div class="hindi-rti">
+                    <p>
+                        सभी प्रकार की जन शिकायतों को प्रस्तुत करने के लिए, कृपया लोक शिकायत अधिकारी, एनआईजीएसटी को लिखें। लोक शिकायत अधिकारी का विवरण निम्नानुसार दिया गया है:</p>
+                    <ul>
+                        <li> लोक शिकायत अधिकारी</li>
+                        <li>श्री. वरून कुमार</li>
+                        <li> उप महासर्वेक्षक</li>
+                        <li> दूरभाषा सं : 040 – 27200188</li>
+                        <li> फैक्स सं : 040 – 27200286</li>
+                        <li> ई – मैल : iism.soi@gov.in</li>
+                    </ul>
+
+
+                </div>
+                <div id="vertical-line"></div>
+                <div class="english-rti">
+
+                    <p>
+                        For submitting all type of public grievances, please write to Public Grievance officer,NIGST. The details of Public Grievance officer given as under:</p>
+                    <h5>CENTRAL PUBLIC INFORMATION OFFICER</h5>
+                    <ul>
+                        <li>PUBLIC GRIEVANCE OFFICER</li>
+                        <li> SHRI G. VARUNA KUMAR</li>
+                        <li> DEPUTY SURVEYOR GENERAL</li>
+                        <li> TEL NO. 040 – 27200188</li>
+                        <li> FAX NO. 040 – 27200286</li>
+                        <li> E-MAIL : iism.soi@gov.in</li>
+                    </ul>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default publicgrievance
