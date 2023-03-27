f858fbf2eeaad464c332ac9cdb836de855c1bb32
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-03-27 13:09:35 +0530

    oi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+
 import Training from './components/NavComponents/Training';
 import Footer from './components/Footer';
 import Copyright from './components/Copyright';
 import About from './about/About';
 import Fixed from './components/Fixed';
 import FacultyGeodesy from './components/faculty/FacultyGeodesy';
-import FacultyPhotogrammetry from './components/faculty/FacultyPhotogrammetry';
-import RightToInformation from './components/RTI/rti'
-import LoginForm from './components/forms/Login';
 import Nigst_campus from './about/Nigst_campus';
-import Faq from './components/FAQ/faq';
-import Privacypolicy from './components/Policies/Privacypolicy';
+
 // import Announcements from './components/Announcements';
 import NIGSTAdmin from './Dashboard/NIGSTAdmin';
 import FacultyAdmin from './Dashboard/FacultyAdmin';
@@ -22,8 +19,6 @@ import Signup from './Dashboard/Signup'
 import NigstCampus from './about/Nigst_campus';
 import OpenPageFromTop from './components/OpenPageFromTop';
 import Board_of_governance from './about/Board_of_governance';
-import Publicgrievance from './components/publicgrievances/Publicgrievance';
-import Websitepolicy from './components/Policies/Websitepolicy';
 
 function App() {
   return (
@@ -36,15 +31,10 @@ function App() {
            <Route path='/training' element={<Training/>} />
            <Route path='/about/introduction' element={<About/>} />
            <Route path='/faculty/geodesy' element={<FacultyGeodesy/>}/>
-           <Route path='/faculty/Photogrammetry' element={<FacultyPhotogrammetry/>}/>
            <Route path='/about/nigst_campus' element={<NigstCampus/>}/>
            <Route path='/training/registration' element={<Signup/>}/>
            <Route path='/about/governance/board_of_governance' element={<Board_of_governance/>} />
-           <Route path='/components/forms/Login' element={<LoginForm/>}/>
-           <Route path='/components/FAQ/faq' element={<Faq/>}/>
-           <Route path='/components/policies/privatepolicy' element={<Privacypolicy/>}/>
-           <Route path='/components/publicgrievances/Publicgrievance' element={<Publicgrievance/>}/>
-           <Route path='/components/Policies/Websitepolicy' element={<Websitepolicy/>}/>
+           <Route path='/board_of_evalution'/>
         </Routes>
         <Footer/>
         <Copyright/>
@@ -54,4 +44,3 @@ function App() {
 }
 
 export default App;
-
