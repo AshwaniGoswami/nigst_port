1c4e56eec01fca5cf04574601ace998cd85e5025
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-03-27 19:40:20 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ import Copyrightpolicy from './components/Policies/Copyrightpolicy';
 import FacultyCarto from './components/faculty/FacultyCarto';
 import FacultyTopo from './components/faculty/FacultyTopo';
 import FacultyGeo from './components/faculty/FacultyGeo';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
@@ -61,13 +62,14 @@ function App() {
 
 
            <Route path='/faq' element={<Faq/>}/>
-           <Route path='/rivatepolicy' element={<Privacypolicy/>}/>
+           <Route path='/privacypolicy' element={<Privacypolicy/>}/>
            <Route path='/publicgrievances' element={<Publicgrievance/>}/>
            <Route path='/websitepolicy' element={<Websitepolicy/>}/>
            <Route path='/faculty/cartography' element={<FacultyCarto/>}/>
            <Route path='/faculty/photogrammetry' element={<FacultyPhotogrammetry/>}/>
            <Route path='/faculty/topographical' element={<FacultyTopo/>}/>
            <Route path='/faculty/geo_ict' element={<FacultyGeo/>}/>
+           {/* <Route path='/gallery' element={<Gallery/>}/> */}
         </Routes>
         <Footer/>
         <Copyright/>
