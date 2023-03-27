cbf2c8357e440e9c0d51d1b25d2b33daa9620d85
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-03-27 18:54:44 +0530

    facultyPages

--- src/App.js
+++ src/App.js
@@ -23,17 +23,6 @@ import OpenPageFromTop from './components/OpenPageFromTop';
 import Board_of_governance from './about/Board_of_governance';
 import Publicgrievance from './components/publicgrievances/Publicgrievance';
 import Websitepolicy from './components/Policies/Websitepolicy';
-import Privacypolicy from './components/Policies/Privacypolicy';
-import Hyperlinkpolicy from './components/Policies/Hyperlinkpolicy';
-import { Feesandpayment } from './components/NavComponents/Feesandpayment/Feesandpayment';
-import Location from './components/NavComponents/Location/Location';
-import Copyrightpolicy from './components/Policies/Copyrightpolicy';
-import FacultyCarto from './components/faculty/FacultyCarto';
-import FacultyTopo from './components/faculty/FacultyTopo';
-import FacultyGeo from './components/faculty/FacultyGeo';
-import Captcha from './components/Captcha';
-import Board_of_evaluation from './about/Board_of_evaluation';
-import Board_of_studies from './about/Board_of_studies';
 
 function App() {
   return (
@@ -52,27 +41,9 @@ function App() {
            <Route path='/about/governance/board_of_governance' element={<Board_of_governance/>} />
            <Route path='/login' element={<LoginForm/>}/>
            <Route path='/components/FAQ/faq' element={<Faq/>}/>
-           <Route path='/Policies/Privacypolicy' element={<Privacypolicy/>}/>
+           <Route path='/components/policies/privatepolicy' element={<Privacypolicy/>}/>
            <Route path='/components/publicgrievances/Publicgrievance' element={<Publicgrievance/>}/>
            <Route path='/components/Policies/Websitepolicy' element={<Websitepolicy/>}/>
-           <Route path='/rti' element={<RightToInformation/>}/>
-           <Route path='/Hyperlinkpolicy' element={<Hyperlinkpolicy/>}/>
-           <Route path='/Feesandpayment' element={<Feesandpayment/>}/>
-           <Route path='/Location' element={<Location/>}/>
-           <Route path='/Copyrightpolicy' element={<Copyrightpolicy/>}/>
-
-
-
-           <Route path='/faq' element={<Faq/>}/>
-           <Route path='/privacypolicy' element={<Privacypolicy/>}/>
-           <Route path='/publicgrievances' element={<Publicgrievance/>}/>
-           <Route path='/websitepolicy' element={<Websitepolicy/>}/>
-           <Route path='/faculty/cartography' element={<FacultyCarto/>}/>
-           <Route path='/faculty/photogrammetry' element={<FacultyPhotogrammetry/>}/>
-           <Route path='/faculty/topographical' element={<FacultyTopo/>}/>
-            <Route path='/faculty/geo_ict' element={<FacultyGeo/>}/>
-            <Route path='/location' element={<Location/>}/>
-           <Route path='/captcha' element={<Captcha/>}/>
         </Routes>
         <Footer/>
         <Copyright/>
