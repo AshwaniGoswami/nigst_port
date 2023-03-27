f97615368ff357499d488a3e14f4e6916cc9bd72
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-03-27 12:01:03 +0530

    fixed

---
+++ src/about/Board_of_governance.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ImgCard from '../components/pageStructure/ImgCard'
+
+const Board_of_governance = () => {
+  return (
+    <div><ImgCard/></div>
+  )
+}
+
+export default Board_of_governance
