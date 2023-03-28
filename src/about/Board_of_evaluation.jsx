d89780d47ac35a7cfeceba3c9a45bf32f8651c25
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-03-28 15:09:58 +0530

    completed

---
+++ src/about/Board_of_evaluation.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ImgCard from '../components/pageStructure/ImgCard';
+
+
+const dataB = {
+    title: 'Board Of Evaluation',
+    members: [
+      {
+        name: 'Shri Sunil Kumar',
+        designation: 'Surveyor General Of India',
+        position: 'Chairman',
+        image: require('../assests/Dr. Srivari Chandrasekhar.jpg')
+      },
+      {
+        name: 'Shri Sunil Kumar',
+        designation: 'CEO',
+        position: 'Board Member',
+        image: require('../assests/Dr. Srivari Chandrasekhar.jpg')
+      },
+      {
+        name: 'Shri Sunil Kumar',
+        designation: 'CEO',
+        position: 'Board Member',
+        image: require('../assests/Dr. Srivari Chandrasekhar.jpg')
+      },
+      {
+        name: 'Shri Sunil Kumar',
+        designation: 'CEO',
+        position: 'Board Member',
+        image: require('../assests/Dr. Srivari Chandrasekhar.jpg')
+      },
+      {
+        name: 'Shri Sunil Kumar',
+        designation: 'CTO',
+        position: 'Board Member',
+        image: require('../assests/Dr. Srivari Chandrasekhar.jpg')
+      }
+    ]
+  };
+  
+const Board_of_evaluation = () => {
+  return (
+    <div><ImgCard title={dataB.title} members={dataB.members} /></div>
+  )
+}
+
+export default Board_of_evaluation
