1038b6ea068a4bb357d44e153d60f5dde37ec26f
Abhishek Paul <phoenixp555@gmail.com>
2023-03-29 21:03:31 +0530

    changes

--- src/components/Buttons/Tables/Tendertable.jsx
+++ src/components/Buttons/Tables/Tendertable.jsx
@@ -70,41 +70,131 @@ function Tendertable() {
           <table>
             <thead>
               <tr>
-                <th colspan="3" style={{ textAlign: "center", backgroundColor: "#ffcb00" }}>Latest Tenders</th>
+                <th colspan="8" style={{ textAlign: "center", backgroundColor: "#ffcb00" }}>Latest Tenders</th>
               </tr>
               <tr>
-                <th>Date</th>
-                <th>Title</th>
-                <th>Description</th>
+              <th>Serial no.</th>
+                        <th>Title</th>
+                        <th>Tendor ref. no.</th>
+                        <th>Description</th>
+                        <th>Corrigendom</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Attachments</th>
               </tr>
             </thead>
 
             <tbody className="main-wrapper" id="Names">
-              <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
-          <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
-          <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
-          <tr>
-            <td>2023-02-27</td>
-            <td>Notice 4</td>
-            <td>tender 2222</td>
-          </tr>
-          <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
+            <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com" target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98162134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
             </tbody>
           </table>
         </div></div>}
@@ -115,41 +205,131 @@ function Tendertable() {
           <table>
             <thead>
               <tr>
-                <th colspan="3" style={{ textAlign: "center", backgroundColor: "#ffcb00" }}>Archived Tenders</th>
+                <th colspan="8" style={{ textAlign: "center", backgroundColor: "#ffcb00" }}>Archived Tenders</th>
               </tr>
               <tr>
-                <th>Date</th>
-                <th>Title</th>
-                <th>Description</th>
+              <th>Serial no.</th>
+                        <th>Title</th>
+                        <th>Tendor ref. no.</th>
+                        <th>Description</th>
+                        <th>Corrigendom</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Attachments</th>
               </tr>
             </thead>
 
             <tbody className="main-wrapper" id="names">
-              <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
-          <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
-          <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
-          <tr>
-            <td>2023-02-27</td>
-            <td>Notice 4</td>
-            <td>tender 2222</td>
-          </tr>
-          <tr>
-            <td>2023-02-26</td>
-            <td>Notice 3</td>
-            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
-          </tr>
+            <tr>
+                        <td>2</td>
+                        <td>Notice 4</td>
+                        <td>98163135</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>lonec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-25</td>
+                        <td>2023-03-27</td>
+                        <td><a href="https://www.google.com" target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98162134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Notice 3</td>
+                        <td>98163134</td>
+                        <td>Lorem ipsum dolor sit.</td>
+                        <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+                        <td>2023-02-26</td>
+                        <td>2023-03-28</td>
+                        <td><a href="https://www.google.com"target="_blank">google.com</a></td>
+                    </tr>
             </tbody>
           </table>
         </div>
