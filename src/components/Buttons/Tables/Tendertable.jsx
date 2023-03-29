bf83c637cbe0c6ca9bb05795a0a698eac862e5c5
Abhishek Paul <phoenixp555@gmail.com>
2023-03-29 17:44:58 +0530

    tender

--- src/components/Buttons/Tables/Tendertable.jsx
+++ src/components/Buttons/Tables/Tendertable.jsx
@@ -1,23 +1,22 @@
-import { Collapse } from "@mui/material";
 import React, { useState } from "react";
 import './Tendertable.css'
 
 function Tendertable() {
   const [showDiv1, setShowDiv1] = useState(true);
   const [showDiv2, setShowDiv2] = useState(false);
-  const [buttonText, setButtonText] = useState("Archieved Tenders");
+  const [buttonText, setButtonText] = useState("Archived Tenders");
 
   const handleButtonClick = () => {
     setShowDiv1(!showDiv1);
     setShowDiv2(!showDiv2);
-    setButtonText(showDiv1 ? "Latest Tenders" : "Archieved Tenders");
+    setButtonText(showDiv1 ? "Latest Tenders" : "Archived Tenders");
   };
   const [searchDate, setSearchDate] = useState("");
 
   const handleInputChange = (event) => {
     setSearchDate(event.target.value);
     const input = event.target.value.toLowerCase();
-    const rows = document.querySelectorAll("#Names tr");
+    const rows = document.querySelectorAll("#Names #names tr");
 
     rows.forEach((row) => {
       const cells = row.querySelectorAll("td");
@@ -86,7 +85,50 @@ function Tendertable() {
             </tbody>
           </table>
         </div></div>}
-      {showDiv2 && <div>Archived Tenders</div>}
+      {showDiv2 && <div><div id="postedNotices" style={{ height: "600px", overflowY: "scroll" }}>
+          <input type="text" id="searchInput" placeholder="Enter date YYYY-MM-DD" value={searchDate} onChange={handleInputChange} />
+
+          <table>
+            <thead>
+              <tr>
+                <th colspan="3" style={{ textAlign: "center", backgroundColor: "#ffcb00" }}>Archived Tenders</th>
+              </tr>
+              <tr>
+                <th>Date</th>
+                <th>Title</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+
+            <tbody className="main-wrapper" id="names">
+              <tr>
+            <td>2023-02-26</td>
+            <td>Notice 3</td>
+            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+          </tr>
+          <tr>
+            <td>2023-02-26</td>
+            <td>Notice 3</td>
+            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+          </tr>
+          <tr>
+            <td>2023-02-26</td>
+            <td>Notice 3</td>
+            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+          </tr>
+          <tr>
+            <td>2023-02-27</td>
+            <td>Notice 4</td>
+            <td>tender 2222</td>
+          </tr>
+          <tr>
+            <td>2023-02-26</td>
+            <td>Notice 3</td>
+            <td>Donec varius justo sit amet sapien lobortis consectetur.</td>
+          </tr>
+            </tbody>
+          </table>
+        </div></div>}
     </div>
   );
 }
