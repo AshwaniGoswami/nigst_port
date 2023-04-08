de0d287b0c7010871e84b71fb0cb021499d1ae29
Abhishek Paul <phoenixp555@gmail.com>
2023-04-08 18:11:33 +0530

    geo spatial page

--- src/components/geospatial.jsx
+++ src/components/geospatial.jsx
@@ -4,20 +4,41 @@ import './geospatial.css'
 
 function Geospatialpolicy() {
   return (
-    <div className="geo-special-container">
-      <div className="geo-special-main-wrapper">
-      <h3>
-  <Link to="../assets/National Geospatial Policy.pdf" target="_blank" rel="noopener noreferrer">
-    National Geospatial Policy <AiOutlineFilePdf style={{ fontSize: 20, color: 'red', marginLeft: 5 }} />
-  </Link>
-</h3>
-<h3>
-  <Link to="../assets/document-49301-New Guidelines on Geospatial Data (1).pdf" target="_blank" rel="noopener noreferrer">
-    New Guidelines on Geospatial Data <AiOutlineFilePdf style={{ fontSize: 20, color: 'red', marginLeft: 5 }} />
-  </Link>
-</h3>
-      </div>
-    </div>
+    <div className="geo-special-main-wrapper">
+  <h4>Geo-Spatial Policy</h4>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Attachments</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><h3>National Geo-Spatial Policy 2022</h3></td>
+        <td>
+          <h3>
+            <Link to="../assests/National Geospatial Policy.pdf" target="_blank" rel="noreferrer">
+            <AiOutlineFilePdf style={{ fontSize: 20, color: 'red', marginLeft: 5 }} />
+            </Link>
+          </h3>
+        </td>
+      </tr>
+      <tr>
+        <td className="color-td">
+          <h3>New Geo-Spatial Data Guidelines 2021</h3>
+        </td>
+        <td className="color-td">
+          <h3>
+            <Link to="../assests/document-49301-New Guidelines on Geospatial Data (1).pdf" target="_blank" rel="noreferrer">
+            <AiOutlineFilePdf style={{ fontSize: 20, color: 'red', marginLeft: 5 }} />
+            </Link>
+          </h3>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
   );
 }
 export default Geospatialpolicy;
