8a3ddaf1a094a149a0f94fd469da224fe4d9912f
Abhishek Paul <phoenixp555@gmail.com>
2023-04-10 17:09:20 +0530

    card carousel

--- src/components/SOI project/soiprojects.jsx
+++ src/components/SOI project/soiprojects.jsx
@@ -1,71 +1,97 @@
-import { useState } from 'react';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+import {useState} from 'react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
-function SimpleSlider() {
 
-const cards = [
-  { 
-    id: 1, 
-    title: 'Card 1', 
-    image: 'https://via.placeholder.com/150', 
-    description: 'Description for Card 1' 
-  },
-  { 
-    id: 2, 
-    title: 'Card 2', 
-    image: 'https://via.placeholder.com/150', 
-    description: 'Description for Card 2' 
-  },
-  { 
-    id: 3, 
-    title: 'Card 3', 
-    image: 'https://via.placeholder.com/150', 
-    description: 'Description for Card 3' 
-  },
-  { 
-    id: 4, 
-    title: 'Card 4', 
-    image: 'https://via.placeholder.com/150', 
-    description: 'Description for Card 4' 
-  },
-  { 
-    id: 5, 
-    title: 'Card 5', 
-    image: 'https://via.placeholder.com/150', 
-    description: 'Description for Card 5' 
-  },
-];
+function SCarousel () {
 
-const Carousel = () => {
-  const [currentCard, setCurrentCard] = useState(0);
+    const [sliderRef, setSliderRef] = useState(null)
 
-  const nextCard = () => {
-    setCurrentCard(currentCard === cards.length - 1 ? 0 : currentCard + 1);
-  };
-
-  const prevCard = () => {
-    setCurrentCard(currentCard === 0 ? cards.length - 1 : currentCard - 1);
-  };
-
-  const displayedCards = [
-    cards[currentCard],
-    cards[(currentCard + 1) % cards.length],
-    cards[(currentCard + 2) % cards.length],
-    cards[(currentCard + 3) % cards.length],
-  ];
+    const sliderSettings = {
+        // removes default buttons
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+      const settings = { 
+        fade: true ,
+        speed: 500, // ms
+        autoplay: false,
+        initialSlide: 2,
+        lazyLoad: true,
+        autoplaySpeed: 3000,
+    }
+  const hotelCards = [
+    {
+      imageSrc:
+        './soiproject/01.jpg',
+      title: '',
+      description: 'Lorem ipsum dolor sit amet, consectur dolori',
+      pricingText: '',
+      features: [''],
+    },
+    {
+      imageSrc:
+        './soiproject/coastal.jpg',
+      title: '',
+      description: 'Lorem ipsum dolor sit amet, consectur dolori',
+      pricingText: '',
+      features: [''],
+    },
+    {
+        imageSrc:
+          './soiproject/nhp.jpg',
+        title: '',
+        description: 'Lorem ipsum dolor sit amet, consectur dolori',
+        pricingText: '',
+        features: [],
+      },
+    {
+      imageSrc:
+        './soiproject/vertical-datum.jpg',
+      title: '',
+      description: '',
+      pricingText: '',
+      features: [''],
+    },
+    {
+      imageSrc:
+        './soiproject/web-gis.jpg',
+      title: '',
+      description: 'Lorem ipsum dolor sit amet, consectur dolori',
+      pricingText: '',
+      features: [
+         ],
+    },
+  ]
 
   return (
-    <div>
-      {displayedCards.map((card) => (
-        <div key={card.id}>
-          <h2>{card.title}</h2>
-          <img src={card.image} alt={card.title} />
-          <p>{card.description}</p>
-        </div>
-      ))}
-      <button onClick={prevCard}>Previous</button>
-      <button onClick={nextCard}>Next</button>
+         <div className='content'>
+             <button onClick={sliderRef?.slickPrev}>
+        <FaArrowLeft />
+      </button>
+      <button onClick={sliderRef?.slickNext}>
+        <FaArrowRight />
+      </button>
+      <Slider  ref={setSliderRef} {...sliderSettings}>
+        {hotelCards.map((card, index) => (
+          <div key={index}>
+            <h2>{card.title}</h2>
+            <img alt={card.title} src={card.imageSrc} width="100" height="100"  />
+            <p>{card.description}</p>
+            <ul>
+              {card.features.map((feature, index) => (
+                <li key={index}>{feature}</li>
+              ))}
+            </ul>
+            <button className='btn'>visit site</button>
+          </div>
+        ))}
+      </Slider>
     </div>
-  );
-};
+  )
 }
-export default SimpleSlider;
+export default  SCarousel;
