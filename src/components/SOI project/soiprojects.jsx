950339185a75aa219489431f89dd209774ad0bf0
Abhishek Paul <phoenixp555@gmail.com>
2023-04-12 16:41:16 +0530

    buttons done

--- src/components/SOI project/soiprojects.jsx
+++ src/components/SOI project/soiprojects.jsx
@@ -91,13 +91,15 @@ function SCarousel() {
   ];
 
   return (
-    <div className='content py-10 bg-gray-300'>
-    <button className="bg-gray-200 text-gray-600 hover:bg-gray rounded-full p-2" onClick={sliderRef?.slickPrev}>
-      <FaArrowLeft />
-    </button>
-    <button className="bg-gray-200 text-gray-600 hover:bg-gray-300 rounded-full p-2" onClick={sliderRef?.slickNext}>
-      <FaArrowRight />
-    </button>
+    <div className='content py-10 '>
+    <div className="mx-3 flex justify-between">
+      <button className="bg-gray-200 text-gray-600 hover:bg-gray-300 rounded-full p-2" onClick={sliderRef?.slickPrev}>
+        <FaArrowLeft />
+      </button>
+      <button className="bg-gray-200 text-gray-600 hover:bg-gray-300 rounded-full p-2" onClick={sliderRef?.slickNext}>
+        <FaArrowRight />
+      </button>
+    </div>
     <Slider className="mt-4 mx-0 grid gap-20 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 " ref={setSliderRef} {...sliderSettings}>
       {hotelCards.map((card, index) => (
         <div key={index} className="p-6 bg-white shadow-md h-full flex flex-col slider">
