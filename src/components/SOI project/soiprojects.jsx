e4719068c1f48750358b05cf2d696fc56a7f2ebb
Abhishek Paul <phoenixp555@gmail.com>
2023-04-12 16:18:54 +0530

    commit home and soi

--- src/components/SOI project/soiprojects.jsx
+++ src/components/SOI project/soiprojects.jsx
@@ -1,6 +1,6 @@
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import "./soiproject/soiproject.css"
+
 import Slider from 'react-slick'
 import { useState } from 'react'
 import img1 from './soiproject/01.jpg';
@@ -9,7 +9,7 @@ import img3 from './soiproject/nhp.jpg';
 import img4 from './soiproject/vertical-datum.jpg';
 import img5 from './soiproject/web-gis.jpg';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
-import { FaCheck } from 'react-icons/fa';
+
 
 
 
@@ -18,13 +18,13 @@ function SCarousel() {
   const [sliderRef, setSliderRef] = useState(null)
 
   const sliderSettings = {
-  arrows: false,
+  arrows: true,
   slidesToShow: 4,
   slidesToScroll: 1,
   infinite: true,
   fade: false,
   speed: 500,
-  autoplay: false,
+  autoplay: true,
   initialSlide: 2,
   lazyLoad: true,
   autoplaySpeed: 3000,
@@ -91,16 +91,16 @@ function SCarousel() {
   ];
 
   return (
-    <div className='content'>
+    <div className='content py-10 bg-gray-300'>
     <button className="bg-gray-200 text-gray-600 hover:bg-gray-300 rounded-full p-2" onClick={sliderRef?.slickPrev}>
       <FaArrowLeft />
     </button>
     <button className="bg-gray-200 text-gray-600 hover:bg-gray-300 rounded-full p-2" onClick={sliderRef?.slickNext}>
       <FaArrowRight />
     </button>
-    <Slider className="mt-4 grid gap-20 grid-cols-1 h-100 sm:grid-cols-2 lg:grid-cols-3" ref={setSliderRef} {...sliderSettings}>
+    <Slider className="mt-4 mx-0 grid gap-20 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 " ref={setSliderRef} {...sliderSettings}>
       {hotelCards.map((card, index) => (
-        <div key={index} className="p-6 bg-white rounded-lg shadow-md h-full flex flex-col">
+        <div key={index} className="p-6 bg-white rounded-lg shadow-md h-full flex flex-col slider">
           <h2 className="text-2xl font-bold mb-2">{card.title}</h2>
           <img alt={card.title} src={card.imageSrc} className="w-full h-64 object-cover mb-4 rounded-lg" />
           <p className="text-gray-700 mb-4 flex-grow">{card.description}</p>
